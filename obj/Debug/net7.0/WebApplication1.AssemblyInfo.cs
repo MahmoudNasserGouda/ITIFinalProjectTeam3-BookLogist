7cca32076603c2ba6ad279d73d2cf71dadcf184b
MahmoudNasserGouda <mahmoudnasser98@gmail.com>
2024-04-16 18:13:25 +0200

    .

--- obj/Debug/net7.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net7.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6d76d2fe99a8735f46aa2e35a38bb5883201acc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
